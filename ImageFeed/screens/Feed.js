e83dd3cc54e5ebfd740defce50766cd7919d047a
Branden Vongphakdy <vbranden_@hotmail.com>
2023-07-13 22:13:18 -0400

    feed wip

---
+++ ImageFeed/screens/Feed.js
@@ -0,0 +1,55 @@
+import {
+    ActivityIndicator,
+    Text,
+    SafeAreaView,
+} from 'react-native';
+import React from 'react';
+
+import { fetchImages } from '../utils/api';
+import CardList from '../components/CardList';
+
+export default class Feed extends React.Component {
+    state = {
+        loading: true,
+        error: false,
+        items: [],
+    };
+
+    async componentDidMount() {
+        try {
+            let items_ = await fetchImages();
+            this.setState({
+                loading: false,
+                items: items_,
+            });
+        } catch (e) {
+            this.setState({
+                loading: false,
+                error: true,
+            });
+        }
+    }
+
+    render() {
+        const { commentsForItem, onPressComments, style } = this.props;
+        const { loading, error, items } = this.state;
+
+        if (loading) {
+            return <ActivityIndicator size="large" />;
+        }
+
+        if (error) {
+            return <Text>Error...</Text>;
+        }
+
+        return (
+            <SafeAreaView style={style}>
+                <CardList
+                    items={items}
+                    commentsForItem={commentsForItem}
+                    onPressComments={onPressComments}
+                />
+            </SafeAreaView>
+        );
+    }
+}
